47fa5aae4176f40c5844b6e3770ff50bfd429c61
Nikita Basargin <nbasargin@web.de>
2023-12-30 17:55:26 +0100

    format everything with prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,15 @@ import { CommonModule } from '@angular/common';
 import { FileListComponent } from './file-list/file-list.component';
 
 @Component({
-    selector: 'ie-root',
-    standalone: true,
-    imports: [CommonModule, FileListComponent],
-    template: `
-        <h1>Welcome to {{ title }}!</h1>
-        <ie-file-list></ie-file-list>
-    `,
-    styles: [],
+  selector: 'ie-root',
+  standalone: true,
+  imports: [CommonModule, FileListComponent],
+  template: `
+    <h1>Welcome to {{ title }}!</h1>
+    <ie-file-list></ie-file-list>
+  `,
+  styles: [],
 })
 export class AppComponent {
-    title = 'inat-editor';
+  title = 'inat-editor';
 }
--- src/app/file-list/file-list.component.ts
+++ src/app/file-list/file-list.component.ts
@@ -4,14 +4,9 @@ import { Component, Input } from '@angular/core';
   selector: 'ie-file-list',
   standalone: true,
   imports: [],
-  template: `
-    <p>
-      file-list works!
-    </p>
-  `,
-  styles: ``
+  template: ` <p>file-list works!</p> `,
+  styles: ``,
 })
 export class FileListComponent {
-  @Input() fileList: Array<{fname: string, selected: boolean}> = [];
-
+  @Input() fileList: Array<{ fname: string; selected: boolean }> = [];
 }
