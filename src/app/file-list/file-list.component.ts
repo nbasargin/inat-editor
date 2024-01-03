d947bdc2813f725cf6913349d50d655e7564268c
Nikita Basargin <nbasargin@web.de>
2024-01-03 22:51:01 +0100

    file list item better variable names

--- src/app/file-list/file-list.component.ts
+++ src/app/file-list/file-list.component.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule, MAT_TOOLTIP_DEFAULT_OPTIONS } from '@angular/material/tooltip';
 
-interface FolderEntry {
-  file: FileSystemDirectoryHandle | FileSystemFileHandle;
+interface FileListItem {
+  fsHandle: FileSystemDirectoryHandle | FileSystemFileHandle;
   icon: string;
 }
 @Component({
@@ -13,29 +13,39 @@ interface FolderEntry {
   imports: [CommonModule, MatIconModule, MatTooltipModule],
   providers: [{ provide: MAT_TOOLTIP_DEFAULT_OPTIONS, useValue: { disableTooltipInteractivity: true } }],
   template: `
-    <div *ngFor="let entry of folderEntries" class="folder-entry">
-      <mat-icon [fontIcon]="entry.icon" class="entry-icon"></mat-icon>
-      <span class="entry-name" [matTooltip]="entry.file.name" [matTooltipShowDelay]="200">{{ entry.file.name }}</span>
+    <div *ngFor="let listItem of fileListItems" class="folder-entry" (click)="clickEntry(listItem.fsHandle)">
+      <mat-icon [fontIcon]="listItem.icon" class="entry-icon"></mat-icon>
+      <span class="entry-name" [matTooltip]="listItem.fsHandle.name" [matTooltipShowDelay]="200">{{
+        listItem.fsHandle.name
+      }}</span>
     </div>
-    <div *ngIf="folderEntries.length === 0" class="folder-entry empty-list-message">
+    <div *ngIf="fileListItems.length === 0" class="folder-entry empty-list-message">
       <mat-icon [fontIcon]="'block'" class="entry-icon"></mat-icon>
       <span class="entry-name">No files</span>
     </div>
   `,
   styleUrl: 'file-list.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FileListComponent {
-  folderEntries: Array<FolderEntry> = [];
+  fileListItems: Array<FileListItem> = [];
 
   @Input() set fileList(list: Array<FileSystemDirectoryHandle | FileSystemFileHandle>) {
-    this.folderEntries = list.map((file) => this.fileToEntry(file));
+    this.fileListItems = list.map((file) => this.fsHandleToListItem(file));
   }
 
-  private fileToEntry(file: FileSystemDirectoryHandle | FileSystemFileHandle): FolderEntry {
-    const icon = file.kind === 'directory' ? 'folder' : 'insert_drive_file';
+  @Output() folderSelected = new EventEmitter<FileSystemDirectoryHandle | undefined>();
+  @Output() fileSelected = new EventEmitter<FileSystemFileHandle | undefined>();
+
+  private fsHandleToListItem(fsHandle: FileSystemDirectoryHandle | FileSystemFileHandle): FileListItem {
+    const icon = fsHandle.kind === 'directory' ? 'folder' : 'insert_drive_file';
     return {
-      file: file,
+      fsHandle: fsHandle,
       icon: icon,
     };
+  }
+
+  clickEntry(file: FileSystemDirectoryHandle | FileSystemFileHandle) {
+    console.log(file);
   }
 }
