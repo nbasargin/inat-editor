56823e4ce46ead8a3aef9946dc0c69ac3933447a
Nikita Basargin <nbasargin@web.de>
2024-01-06 17:32:40 +0100

    move image loading into a separate class

--- src/app/image-editor/image-editor.component.ts
+++ src/app/image-editor/image-editor.component.ts
@@ -1,41 +1,27 @@
 import { ChangeDetectionStrategy, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FsItem } from '../fs-item';
+import { ImageLoader } from '../image-loader';
 
 @Component({
   selector: 'ie-image-editor',
   standalone: true,
-  imports: [],
-  template: ` <img #mainImage class="main-image" /> `,
+  imports: [CommonModule],
+  template: ` <img *ngIf="imageLoader" class="main-image" [src]="imageLoader.asyncObjectURL | async" /> `,
   styleUrl: 'image-editor.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ImageEditorComponent {
-  @ViewChild('mainImage') mainImage!: ElementRef<HTMLImageElement>;
+  imageLoader: ImageLoader | null = null;
 
   @Input() set selectedFile(fsItem: FsItem<FileSystemFileHandle> | null) {
-    this.unloadImage();
+    if (this.imageLoader) {
+      this.imageLoader.destroy();
+      this.imageLoader = null;
+    }
     if (!fsItem) {
       return;
     }
-    this.loadImage(fsItem.handle);
-  }
-
-  private currentDataURL: string | null = null;
-
-  unloadImage() {
-    if (this.currentDataURL) {
-      this.mainImage.nativeElement.src = '';
-      URL.revokeObjectURL(this.currentDataURL);
-      this.currentDataURL = null;
-    }
-  }
-
-  async loadImage(handle: FileSystemFileHandle) {
-    const file = await handle.getFile();
-    if (file.type !== 'image/jpeg' && file.type !== 'image/png') {
-      return;
-    }
-    this.currentDataURL = URL.createObjectURL(file);
-    this.mainImage.nativeElement.src = this.currentDataURL;
+    this.imageLoader = new ImageLoader(fsItem.handle);
   }
 }
---
+++ src/app/image-loader.ts
@@ -0,0 +1,34 @@
+/**
+ * Loads an image as object URL given a file handle.
+ * Only JPG and PNG images allowed.
+ * Object URL can be asynchronously retrieved from asyncObjectURL.
+ * This loader should be destroyed when the image is no longer needed.
+ */
+export class ImageLoader {
+  asyncObjectURL: Promise<string | null>;
+  private destroyed = false;
+  private objectURL: string | null = null;
+
+  constructor(public readonly handle: FileSystemFileHandle) {
+    this.asyncObjectURL = this.handle.getFile().then((file) => {
+      if (file.type !== 'image/jpeg' && file.type !== 'image/png') {
+        return null;
+      }
+      if (this.destroyed) {
+        return null;
+      }
+      this.objectURL = URL.createObjectURL(file);
+      console.log('created object url', this.objectURL);
+      return this.objectURL;
+    });
+  }
+
+  destroy() {
+    this.destroyed = true;
+    if (this.objectURL) {
+      console.log('destroying object url', this.objectURL);
+      URL.revokeObjectURL(this.objectURL);
+      this.objectURL = null;
+    }
+  }
+}
