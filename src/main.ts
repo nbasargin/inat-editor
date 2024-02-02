54e435685a69e6b5193462cb117505e2b943652d
Nikita Basargin <nbasargin@web.de>
2024-02-02 22:18:37 +0100

    enable animations

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, { providers: [importProvidersFrom([BrowserAnimationsModule])] }).catch((err) =>
+  console.error(err),
+);
